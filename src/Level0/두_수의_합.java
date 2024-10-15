d5888a6c318c4c310388bdb08e8ed91d01c29924
wintee-laptop <b12e34e56@naver.com>
2024-10-16 00:59:22 +0200

    Level 0 - 사칙연산

---
+++ src/Level0/두_수의_합.java
@@ -0,0 +1,7 @@
+package Level0;
+
+public class 두_수의_합 {
+    public int solution(int num1, int num2) {
+        return num1 + num2;
+    }
+}
---
+++ src/Level0/몫_구하기.java
@@ -0,0 +1,7 @@
+package Level0;
+
+public class 몫_구하기 {
+    public int solution(int num1, int num2) {
+        return num1 / num2;
+    }
+}
